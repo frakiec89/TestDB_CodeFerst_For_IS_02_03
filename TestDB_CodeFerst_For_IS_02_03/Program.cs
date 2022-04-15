9661fc45fb1434f065ca89e444d36e18b5388ba7
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-04-15 14:25:17 +0400

    Создали Меню Программы

--- TestDB_CodeFerst_For_IS_02_03/Program.cs
+++ TestDB_CodeFerst_For_IS_02_03/Program.cs
@@ -2,11 +2,97 @@
 
 namespace TestDB_CodeFerst_For_IS_02_03
 {
-    internal class Program
+    internal class Program // это view
     {
-        static void Main(string[] args)
+        static string getRead = "get"; // команды
+        static string addRead = "add";
+        static string exit = "exit";
+        static string AllRemove = "all-dell";
+        static string AllReplace = "all-replace";
+        static string AllRemoveForerer = "all-dell-forever";
+        static string removeReader = "remove";
+        static string getStatus = "get-status";
+        static string getReadStatus = "get-reader-status";
+        static void Main(string[] args)  // ссылка на  гит  https://github.com/frakiec89/TestDB_CodeFerst
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Программа -- мои  записки --- ");
+
+            while (true)
+            {
+                Console.WriteLine("---- доступные  команды ---- ");
+                Console.WriteLine($"для того что  бы  получит  список  записей введите \"{getRead}\"");
+                Console.WriteLine($"для того что  бы  добавить  запись введите \"{addRead}\"");
+                Console.WriteLine($"для того что  бы  получить  список статусов введите \"{getStatus}\"");
+                Console.WriteLine($"для того что  бы  получить  список  записей  по  одному статусу введите \"{getReadStatus}\"");
+
+                Console.WriteLine($"для того что  бы  удалить запись введите \"{removeReader}\"");
+                Console.WriteLine($"для того что  бы  удалить  все записи введите \"{AllRemove}\"");
+                Console.WriteLine($"для того что  бы  вернуть  все записи введите \"{AllReplace}\"");
+                Console.WriteLine();
+                Console.WriteLine($"для того что  бы  удалить все записи навсегда введите \"{AllRemoveForerer}\"");
+                Console.WriteLine($"для того что  бы  выйти из программы введите \"{exit}\"");
+                Console.WriteLine("----   сгк продакшен   ---- ");
+
+
+                switch (Console.ReadLine().ToLower().TrimStart().TrimEnd())
+                {
+                    case "get": GetRead(); break;
+                    case "add": AddRead(); break;
+                    case "exit":; return;
+                    case "all-dell": AllDell(); break;
+                    case "all-replace": AllReplaceMetod(); break;
+                    case "all-dell-forever": AllReplaceMetodForever(); break;
+                    case "remove": Remove(); break;
+                    case "get-status": GetStatus(); break;
+                    case "get-reader-status": GetReaderStatus(); break;
+
+
+                    default:
+                        Console.WriteLine("не корректная команда");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void GetReaderStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GetStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AllReplaceMetodForever()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AllReplaceMetod()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AllDell()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddRead()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GetRead()
+        {
+            throw new NotImplementedException();
         }
     }
 }
