ad1057cbb5f8a8627e097d9ebeb550158079531e
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-04-15 14:21:45 +0400

    Формирование  базы данных из модели Контекст  соединения  с  БД

---
+++ TestDB_CodeFerst_For_IS_02_03/DB/Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestDB_CodeFerst_For_IS_02_03.DB
+{
+    /// <summary>
+    /// модель базы данных
+    /// </summary>
+    public class Record
+    {
+        public int RecordId { get; set; }
+        public string Content { get; set; }
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Дата  записи
+        /// </summary>
+        public DateTime DateRecord { get; set; }
+
+        /// <summary>
+        /// Вывод клиенту
+        /// </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
