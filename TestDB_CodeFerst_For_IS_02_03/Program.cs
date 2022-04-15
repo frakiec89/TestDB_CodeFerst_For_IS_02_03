478b315e0bd5db0ae984b9d9034a14fcede72c1a
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-04-15 10:23:21 +0400

    Добавьте файлы проекта.

---
+++ TestDB_CodeFerst_For_IS_02_03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDB_CodeFerst_For_IS_02_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
